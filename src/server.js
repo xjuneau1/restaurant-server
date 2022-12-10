46fb8e477bce0db5b950f15412efacd51b9d8a10
Xander Juneau <xanderjuneau@gmail.com>
2022-12-10 18:28:29 -0500

    changed app listen back to use env

--- src/server.js
+++ src/server.js
@@ -7,7 +7,8 @@ knex.migrate
   .latest()
   .then((migrations) => {
     console.log("migrations", migrations);
-    app.listen("80", "192.168.1.199", listener);
+    app.listen(PORT, listener);
+    // app.listen("80", "192.168.1.199", listener);
   })
   .catch((error) => {
     console.error(error);
@@ -15,5 +16,5 @@ knex.migrate
   });
 
 function listener() {
-  console.log(`Listening on Port 80!`);
+  console.log(`Listening on Port ${PORT}!`);
 }
